a25ff1ec2155af994c2ae31c216ee27c73890b3a
Andrew <and.razumov2014@gmail.com>
2022-04-04 22:01:12 +0800

    fragments, view models, snap pager and alert dialog for weather app

--- CurrentWeatherDataBinding/app/src/main/java/com/example/currentweatherdatabinding/MainActivity.kt
+++ CurrentWeatherDataBinding/app/src/main/java/com/example/currentweatherdatabinding/MainActivity.kt
@@ -2,16 +2,31 @@ package com.example.currentweatherdatabinding
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.TextView
+import android.widget.*
+import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.LifecycleOwner
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.RecyclerView
+import com.example.currentweatherdatabinding.alertDialog.DialogViewModel
+import com.example.currentweatherdatabinding.alertDialog.DisplayingMode
+import com.example.currentweatherdatabinding.alertDialog.SettingsDialog
+import com.example.currentweatherdatabinding.citiesRecycler.CitiesAdapter
+import com.example.currentweatherdatabinding.citiesRecycler.ScrollPagerListener
 import com.example.currentweatherdatabinding.databinding.ActivityMainBinding
+import com.example.currentweatherdatabinding.fragments.InformativeFragment
+import com.example.currentweatherdatabinding.fragments.SimpleFragment
+import com.example.currentweatherdatabinding.fragments.WeatherViewModel
 import com.example.currentweatherdatabinding.service.FetchingWeatherEvent
 import com.example.currentweatherdatabinding.service.WeatherFetcherService
 import com.example.currentweatherdatabinding.utils.WeatherUtils
@@ -19,27 +34,70 @@ import kotlinx.coroutines.*
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
 
-class MainActivity : AppCompatActivity(),  AdapterView.OnItemSelectedListener{
+class MainActivity : AppCompatActivity() {
 
-    lateinit var spinner: Spinner
-    lateinit var cityNameView: TextView
-    lateinit var dBind: ActivityMainBinding
+    private lateinit var dBind: ActivityMainBinding
     @SuppressLint("SimpleDateFormat")
     lateinit var API_KEY: String
     private var cityName = ""
-    lateinit var weatherFetcherService: WeatherFetcherService
+    private lateinit var weatherFetcherService: WeatherFetcherService
+    private lateinit var settingsBtn: ImageView
+    lateinit var settingsDialog: SettingsDialog
+    private val dialogViewModel: DialogViewModel by viewModels()
+    private val weatherViewModel: WeatherViewModel by viewModels()
+    private lateinit var citiesRecyclerView: RecyclerView
+    lateinit var fragmentManager: FragmentManager
+    lateinit var fragmentTransaction: FragmentTransaction
+    lateinit var informativeFragment: Fragment
+    lateinit var simpleFragment: Fragment
+    lateinit var citiesNames: Array<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         dBind = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        spinner = findViewById(R.id.cities_dropdown)
-        cityNameView = findViewById(R.id.city_name)
+        settingsBtn = findViewById(R.id.setting_btn)
+        citiesRecyclerView = findViewById(R.id.cities_recycler)
+        citiesNames = resources.getStringArray(R.array.cities)
         API_KEY = getString(R.string.API_KEY)
+        cityName = citiesNames[0]
 
         weatherFetcherService = WeatherFetcherService()
-        initSpinner()
+        dialogViewModel.displayingMode.value = DisplayingMode.INFORMATIVE
+        settingsDialog = SettingsDialog(this)
+        initRecyclerView()
+
+        fragmentManager = supportFragmentManager
+        initFragments()
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.inflateMenu(R.menu.lang_menu)
+        toolbar.setTitleTextColor(Color.WHITE)
+        toolbar.setOnMenuItemClickListener { item ->
+            handleChangeLocalization(item)
+            true
+        }
+
         EventBus.getDefault().register(this)
+        startFetchingWeather()
+
+        dialogViewModel.displayingMode.observe( this) {
+            fragmentTransaction = fragmentManager.beginTransaction()
+
+            if (it == DisplayingMode.INFORMATIVE && !informativeFragment.isVisible) {
+                fragmentTransaction.replace(R.id.fragment_container_view, informativeFragment).commit()
+            } else if (it == DisplayingMode.SIMPLE && !simpleFragment.isVisible) {
+                fragmentTransaction.replace(R.id.fragment_container_view, simpleFragment).commit()
+            }
+        }
+    }
+
+    fun handleChangeLocalization(item: MenuItem) {
+        Log.d("Menu item", item.toString())
+
+    }
+
+    fun onSettingsButtonClick(v: View) {
+        settingsDialog.show(supportFragmentManager, "SettingsModal")
     }
 
     override fun onResume() {
@@ -57,36 +115,39 @@ class MainActivity : AppCompatActivity(),  AdapterView.OnItemSelectedListener{
         super.onDestroy()
     }
 
-    private fun initSpinner() {
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.cities,
-            android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            spinner.adapter = adapter
-        }
-        spinner.onItemSelectedListener = this
-    }
+    private fun initFragments() {
+        fragmentTransaction = fragmentManager.beginTransaction()
+        informativeFragment = InformativeFragment()
+        simpleFragment = SimpleFragment()
 
-    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        if (parent != null) {
-            cityName = parent.getItemAtPosition(position).toString()
-            cityNameView.text = cityName
-        }
-
-        startFetchingWeather()
-    }
-
-    override fun onNothingSelected(parent: AdapterView<*>?) {
-        // nothing changes
+        fragmentManager.beginTransaction()
+            .add(R.id.fragment_container_view, if (dialogViewModel.displayingMode.value == DisplayingMode.INFORMATIVE) informativeFragment else simpleFragment)
+            .commit()
     }
 
     @OptIn(DelicateCoroutinesApi::class)
-    fun onUpdateClick(v: View) {
+    private fun pagerItemChangeListener(newPosition: Int) {
         GlobalScope.launch(Dispatchers.IO) {
-            dBind.weatherData = WeatherUtils.loadWeather(cityName, API_KEY)
+            cityName = citiesNames[newPosition]
+            val weatherData = WeatherUtils.loadWeather(citiesNames[newPosition], API_KEY)
+            if (weatherData != null) {
+                dBind.weatherData = weatherData
+                weatherViewModel.weatherData.postValue(weatherData)
+            }
         }
+    }
+
+    private fun initRecyclerView() {
+        val citiesAdapter = CitiesAdapter(citiesNames, this)
+        citiesRecyclerView.adapter = citiesAdapter
+
+        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        citiesRecyclerView.layoutManager = layoutManager
+
+        val pagerHelper = PagerSnapHelper()
+        pagerHelper.attachToRecyclerView(citiesRecyclerView)
+        val scrollPagerHelper = ScrollPagerListener(pagerHelper, ::pagerItemChangeListener)
+        citiesRecyclerView.addOnScrollListener(scrollPagerHelper)
     }
 
     private fun startFetchingWeather() {
@@ -100,5 +161,6 @@ class MainActivity : AppCompatActivity(),  AdapterView.OnItemSelectedListener{
     @Subscribe
     fun onFetchingWeatherEvent(fetchingWeatherEvent: FetchingWeatherEvent) {
         dBind.weatherData = fetchingWeatherEvent.weatherData
+        weatherViewModel.weatherData.postValue(fetchingWeatherEvent.weatherData)
     }
 }
--- CurrentWeatherDataBinding/app/src/main/java/com/example/currentweatherdatabinding/utils/WeatherUtils.kt
+++ CurrentWeatherDataBinding/app/src/main/java/com/example/currentweatherdatabinding/utils/WeatherUtils.kt
@@ -1,11 +1,14 @@
 package com.example.currentweatherdatabinding.utils
 
 import android.annotation.SuppressLint
+import android.util.Log
 import com.example.currentweatherdatabinding.R
 import com.example.currentweatherdatabinding.dataclasses.MainWeather
 import com.example.currentweatherdatabinding.dataclasses.WeatherData
 import com.example.currentweatherdatabinding.dataclasses.Wind
 import com.google.gson.Gson
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.RequestCreator
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -26,6 +29,7 @@ class WeatherUtils {
         @SuppressLint("SimpleDateFormat")
         val formatter = SimpleDateFormat("dd MM yyyy HH:mm")
         private val gson = Gson()
+        private val picasso = Picasso.get()
 
         fun loadWeather(cityName: String, API_KEY: String): WeatherData? {
             val weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric&lang=ru";
@@ -39,19 +43,45 @@ class WeatherUtils {
 
                 val main = gson.fromJson(data.get("main").toString(), MainWeather::class.java)
                 val speed = (data.get("wind") as JSONObject).get("speed").toString().toDouble()
-                val windDirection = defineWindDirection((data.get("wind") as JSONObject).get("deg").toString().toInt())
-                val wind = Wind(speed, windDirection)
+                val speed_pic = defineWindSpeed(speed)
+                val (direction, picture) = defineWindDirection((data.get("wind") as JSONObject).get("deg").toString().toInt())
+                val wind = Wind(speed, direction, picture, speed_pic)
                 val description = ((data.get("weather") as JSONArray)[0] as JSONObject).get("description").toString()
+                val icon_code = ((data.get("weather") as JSONArray)[0] as JSONObject).get("icon").toString()
+                val icon = loadPicture(icon_code)
                 val date = formatter.format(Date(Timestamp(data.get("dt").toString().toLong()).time * 1000))
 
-                return WeatherData(description, main, wind, date)
+                return WeatherData(description, main, wind, date, icon)
             } catch (e: IOException) {
-                //
+                return null
             }
             return null
         }
 
-        private fun defineWindDirection(azimut: Int): String {
+        private fun loadPicture(icon_code: String): RequestCreator? {
+            val iconURL = "http://openweathermap.org/img/wn/${icon_code}@2x.png"
+            return try {
+                picasso.load(iconURL)
+            } catch (e: IOException) {
+                Log.d("PIC_LOADING", "error while loading picture")
+                null
+            }
+        }
+
+        private fun defineWindSpeed(speed: Double): Int {
+            if (speed < 0.2) {
+                return R.drawable.ic_wind_small
+            } else if (speed < 5) {
+                return R.drawable.ic_wind_mild
+            } else if (speed < 10) {
+                return R.drawable.ic_wind_medium
+            } else if (speed < 17) {
+                return R.drawable.ic_wind_strong
+            }
+            return R.drawable.ic_wind_huge
+        }
+
+        private fun defineWindDirection(azimut: Int): Pair<String, Int> {
             val azimuts = intArrayOf(0, 45, 90, 135, 180, 225, 270, 315, 359)
             for (i in azimuts.indices) {
                 azimuts[i] = abs(azimuts[i] - azimut)
@@ -64,18 +94,47 @@ class WeatherUtils {
                     minIndex = i
                 }
             }
-            return when (minIndex) {
-                0 -> "c"
-                1 -> "c/в"
-                2 -> "в"
-                3 -> "ю/в"
-                4 -> "ю"
-                5 -> "ю/з"
-                6 -> "з"
-                7 -> "с/з"
-                8 -> "c"
-                else -> "c"
+            var direction: String = ""
+            var picture: Int = -1
+            when (minIndex) {
+                0, 8 -> {
+                    direction = "c"
+                    picture = R.drawable.ic_up
+                }
+                1 -> {
+                    direction = "c/в"
+                    picture = R.drawable.ic_up
+                }
+                2 -> {
+                    direction = "в"
+                    picture = R.drawable.ic_right
+                }
+                3 -> {
+                    direction = "ю/в"
+                    picture = R.drawable.ic_right
+                }
+                4 -> {
+                    direction = "ю"
+                    picture = R.drawable.ic_down
+                }
+                5 -> {
+                    direction = "ю/з"
+                    picture = R.drawable.ic_down
+                }
+                6 -> {
+                    direction = "з"
+                    picture = R.drawable.ic_right
+                }
+                7 -> {
+                    direction = "с/з"
+                    picture = R.drawable.ic_right
+                }
+                else -> {
+                    direction = "c"
+                    picture = R.drawable.ic_up
+                }
             }
+            return Pair(direction, picture)
         }
     }
 }
